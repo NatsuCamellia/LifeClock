27a8a355765951b4cbe3522963c32af667fedb2f
NatsuCamellia <natsucamellia@gmail.com>
2024-08-24 20:12:37 +0800

    Make navigation bar transparent

--- app/src/main/java/net/natsucamellia/lifeclock/MainActivity.kt
+++ app/src/main/java/net/natsucamellia/lifeclock/MainActivity.kt
@@ -4,17 +4,17 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.graphics.Color
 import android.os.Bundle
 import android.os.IBinder
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import net.natsucamellia.lifeclock.services.TimerService
 import net.natsucamellia.lifeclock.ui.model.LifeClockModel
 import net.natsucamellia.lifeclock.ui.nav.NavContainer
@@ -25,7 +25,9 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
 
         startTimerService()
         setContent {
